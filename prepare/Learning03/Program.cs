18fafa17317ef076821d115193ffab5c4540827e
epicemrys <emrysweb@outlook.com>
2024-10-05 12:49:49 +0100

    Encapsulation added

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
-class Program
+namespace FractionExample
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Learning03 World!");
+        static void Main(string[] args)
+        {
+            Fractions.Fraction fraction1 = new Fractions.Fraction();  // 1/1
+            Fractions.Fraction fraction2 = new Fractions.Fraction(5); // 5/1
+            Fractions.Fraction fraction3 = new Fractions.Fraction(6, 7); // 6/7
+
+            Console.WriteLine(fraction1.GetFractionString());
+            Console.WriteLine(fraction2.GetFractionString());
+            Console.WriteLine(fraction3.GetFractionString());
+
+            Console.WriteLine(fraction1.GetDecimalValue());
+            Console.WriteLine(fraction2.GetDecimalValue());
+            Console.WriteLine(fraction3.GetDecimalValue());
+        }
     }
 }
