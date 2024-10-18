703e7fa41be58c7c6769ba172ca329eddb83763f
epicemrys <emrysweb@outlook.com>
2024-10-18 15:15:14 +0100

    Video Comment Added

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -67,14 +67,22 @@ class Program
         // Create videos
         var video1 = new Video("How to read the scriptures", "Charlie", 120);
         var video2 = new Video("My Book of Mormon Experience", "Emrys", 180);
+        var video3 = new Video("The love of Christ for you", "Vivi", 132);
         
         // Add comments to the videos
         video1.AddComment(new Comment("Ukoh", "Great video!"));
         video1.AddComment(new Comment("UtahViewer", "Interesting content."));
+        video1.AddComment(new Comment("Debbie", "You are right about the Love of Christ. "));
         video2.AddComment(new Comment("BYU456", "Well done."));
+        video2.AddComment(new Comment("Joseph", "I have a strong testimony. "));
+        video2.AddComment(new Comment("Pearl", "I love the Lord with all my heart. "));
+        video3.AddComment(new Comment("Burton", "It encompassess all understanding. "));
+        video3.AddComment(new Comment("User2024", "I love the words on the 24th seconds. "));
+        video3.AddComment(new Comment("Laura", "I love the Church of Jesus Christ. "));
+
         
         // Display video details
-        var videos = new List<Video> { video1, video2 };
+        var videos = new List<Video> { video1, video2, video3 };
         foreach (var video in videos)
         {
             video.DisplayVideoDetails();
